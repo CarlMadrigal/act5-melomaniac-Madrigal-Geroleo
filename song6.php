206bb2f993053114348c97ce6c75011124a2309b
CarlMadrigal <carlmadrigal65@gmail.com>
2024-03-07 20:43:39 +0800

    Song6.php Added

---
+++ song6.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Heaven Knows</title>
+    <style>
+        body {
+            font-family: Arial;
+            background-color: #f0f0f0;
+        }
+        h1, h5 {
+            margin-top: 20px;
+        }
+        hr {
+            border: 3px solid #ccc;
+        }
+    </style>
+</head>
+<body>
+    <h1>Heaven Knows</h1>
+    <h5>By: Orange & Lemons</h5>
+    <hr>
+    <?php
+    echo "<pre>";
+    echo "<sub>AM7 Am E B7 G B7</sub>\n";
+    echo "\n";
+    echo "<sub>EM7 Asus2 x3</sub>\n";
+    echo "<sub>F#m B</sub>\n";
+    echo "\n";
+    echo "\n";
+    echo "           <sub>E              Asus2</sub>\n";
+    echo "There are times when I’m lying in my bed\n";
+    echo "      <sub>E                        Asus2</sub>\n";
+    echo "How I bellow and cry from this stupid game\n";
+    echo "       <sub>E             Asus2                   E</sub>\n";
+    echo "And my eyes are like windshields on a rainy day\n";
+    echo "                    <sub>Asus2</sub>\n";
+    echo "Almost rubbed down, swelling, as I keep on\n";
+    echo "\n";
+    echo "\n";
+    echo "[Refrain]\n";
+    echo "\n";
+    echo "<sub>G#m                Asus2</sub>\n";
+    echo "Dipping my face in these cold hands of mine\n";
+    echo "<sub>G#m                F#m     B    E</sub>\n";
+    echo "Heaven knows how embittered I am\n";
+    echo "\n";
+    echo "\n";
+    echo "                      <sub>Asus2           G#m</sub>\n";
+    echo "‘Cause this angel has flown away from me\n";
+    echo "              <sub>F#m     B    E</sub>\n";
+    echo "Leaving me in drunken misery\n";
+    echo "              <sub>Asus2                 G#m</sub>\n";
+    echo "I should have clipped her wings and made her mine\n";
+    echo "    <sub>F#m       B</sub>\n";
+    echo "For all eternity\n";
+    echo "                   <sub>Asus2           G#m</sub>\n";
+    echo "Now this angel has flown away from me\n";
+    echo "                  <sub>F#m         B        E</sub>\n";
+    echo "Thought I had the strength to set her free\n";
+    echo "           <sub>Asus2           G#m</sub>\n";
+    echo "Did what I did because I love her so\n";
+    echo "<sub>DM7                             FM7</sub>\n";
+    echo "Will she ever find her way back home to me\n";
+    echo "    <sub>EM7  Asus2  F#m  B</sub>\n";
+    echo "Aah…\n";
+    echo "\n";
+    echo "\n";
+    echo "       <sub>E                  Asus2</sub>\n";
+    echo "I’m so tired, I feel like catching forty winks\n";
+    echo "      <sub>E              Asus2</sub>\n";
+    echo "Being up all night in this elbow room\n";
+    echo "       <sub>Am              E</sub>\n";
+    echo "That puts me in a trance\n";
+    echo "      <sub>Asus2                    E      Asus2</sub>\n";
+    echo "Where hopes and dreams come true\n";
+    echo "       <sub>E        Asus2           E        Asus2</sub>\n";
+    echo "Now my lips are burning and my eyes are hurting\n";
+    echo "          <sub>G#m                  Asus2</sub>\n";
+    echo "From this fuse I mixed till I light another\n";
+    echo "<sub>G#m               Asus2</sub>\n";
+    echo "Cigarette just to pass my time, oh\n";
+    echo "<sub>G#m                F#m     B   E</sub>\n";
+    echo "Heaven knows how embittered I am\n";
+    echo "\n";
+    echo "\n";
+    echo "                      <sub>Asus2           G#m</sub>\n";
+    echo "‘Cause this angel has flown away from me\n";
+    echo "              <sub>F#m     B    E</sub>\n";
+    echo "Leaving me in drunken misery\n";
+    echo "              <sub>Asus2                 G#m</sub>\n";
+    echo "I should have clipped her wings and made her mine\n";
+    echo "    <sub>F#m       B</sub>\n";
+    echo "For all eternity\n";
+    echo "                   <sub>Asus2           G#m</sub>\n";
+    echo "Now this angel has flown away from me\n";
+    echo "                  <sub>F#m         B        E</sub>\n";
+    echo "Thought I had the strength to set her free\n";
+    echo "           <sub>Asus2           G#m</sub>\n";
+    echo "Did what I did because I love her so\n";
+    echo "<sub>DM7                             FM7</sub>\n";
+    echo "Will she ever find her way back home to me\n";
+    echo "   <sub>EM7  CM7 D Am E</sub>\n";
+    echo "Aah…\n";
+    echo "</pre>";
+    ?>
+</body>
+</html>
