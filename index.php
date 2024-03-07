e0c1da362d8ccaef8cf251f70e1e87e675e267a0
CarlMadrigal <carlmadrigal65@gmail.com>
2024-03-07 20:42:24 +0800

    Index.php Added

---
+++ index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dabitify</title>
+    <link rel="icon" href="Image/logo.png">
+    <style>
+
+        body {
+            font-family: Arial;
+            background-color: #f0f0f0;
+        }
+        h1 {
+            text-align: center;
+            margin-top: 20px;
+        }
+        hr {
+            margin: 20px auto;
+            width: 80%;
+            border: 3px solid silver;
+        }
+        .link {
+            display: block;
+            text-decoration: none;
+            color: black;
+            padding: 10px;
+        }
+        .link:hover {
+            background-color: aliceblue;
+        }
+        .link img {
+            height: 40px;
+            width: 40px;
+            margin-right: 10px;
+            float: left;
+        }
+        .details {
+            margin-top: 5px;
+        }
+        .title {
+            margin: 0;
+            font-size: 1.2em;
+        }
+        .artist {
+            margin: 0;
+            font-size: 0.9em;
+            color: darkslategrey;
+        }
+    </style>
+</head>
+<body>
+    <h1>DABITIFY</h1>
+    <hr>
+
+    <a class="link" href="song1.php">
+        <img src="Image/song1cover.jpg" alt="Song 1 Cover">
+        <div class="details">
+            <h3 class="title">Pluto Project</h3>
+            <p class="artist">Song by Rex Orange County</p>
+        </div>
+    </a>
+
+    <a class="link" href="song2.php">
+        <img src="Image/song2cover.jpg" alt="Song 2 Cover">
+        <div class="details">
+            <h3 class="title">Double Take</h3>
+            <p class="artist">Song by Dhruv</p>
+        </div>
+    </a>
+
+    <a class="link" href="song3.php">
+        <img src="Image/song3cover.jpg" alt="Song 3 Cover">
+        <div class="details">
+            <h3 class="title">Locked Out of Heaven</h3>
+            <p class="artist">Song by Bruno Mars</p>
+        </div>
+    </a>
+
+    <a class="link" href="song4.php">
+        <img src="Image/song4cover.jpg" alt="Song 4 Cover">
+        <div class="details">
+            <h3 class="title">Starboy</h3>
+            <p class="artist">Song by The Weekend, Draft Punk</p>
+        </div>
+    </a>
+
+    <a class="link" href="song5.php">
+        <img src="Image/song5cover.jpg" alt="Song 5 Cover">
+        <div class="details">
+            <h3 class="title">Sugar</h3>
+            <p class="artist">Song by Brockhampton</p>
+        </div>
+    </a>
+
+    <a class="link" href="song6.php">
+        <img src="Image/song6cover.jpg" alt="Song 6 Cover">
+        <div class="details">
+            <h3 class="title">Heaven Knows</h3>
+            <p class="artist">Song by Lemons & Orange</p>
+        </div>
+    </a>
+
+    <a class="link" href="song7.php">
+        <img src="Image/song7cover.jpg" alt="Song 7 Cover">
+        <div class="details">
+            <h3 class="title">Sinta</h3>
+            <p class="artist">Song by The Clubs</p>
+        </div>
+    </a>
+
+    <a class="link" href="song8.php">
+        <img src="Image/song8cover.jpg" alt="Song 8 Cover">
+        <div class="details">
+            <h3 class="title">Leoonora</h3>
+            <p class="artist">Song by Sugarcane</p>
+        </div>
+    </a>
+
+    <a class="link" href="song9.php">
+        <img src="Image/song9cover.jpg" alt="Song 9 Cover">
+        <div class="details">
+            <h3 class="title">Happiness</h3>
+            <p class="artist">Song by Rex Orange Country</p>
+        </div>
+    </a>
+</body>
+</html>
