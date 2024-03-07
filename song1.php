ca050069243fa51c04f339f4c100ba078dd828b4
CarlMadrigal <carlmadrigal65@gmail.com>
2024-03-07 20:42:40 +0800

    Song1.php Added

---
+++ song1.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pluto Project</title>
+    <style>
+        body {
+            font-family: Arial;
+            background-color: #f0f0f0;
+        }
+        h1, h5 {
+            margin-top: 20px;
+        }
+        hr {
+            border: 3px solid #ccc;
+        }
+    </style>
+
+</head>
+<body>
+    <h1>Pluto Project</h1>
+    <h5>By: Rex Orange Country</h5>
+    <hr>
+    <?php
+    echo "<pre>";
+    echo "<sub>C</sub>\n";
+    echo "The great protector\n";
+    echo "                             <sub>Em7</sub>\n";
+    echo "Is that what I'm supposed to be?\n";
+    echo "        <sub>Fmaj7</sub>\n";
+    echo "What if all this counts for nothing\n";
+    echo "                            <sub>F#m7b5</sub>\n";
+    echo "Everything I thought I'd be?\n";
+    echo "          <sub>G7</sub>\n";
+    echo "What if by the time I realize\n";
+    echo "          <sub>G7sus4         G7</sub>\n";
+    echo "It's too far behind to see?\n";
+    echo "<sub>C</sub>\n";
+    echo "Seventy-mil projector\n";
+    echo "                      <sub>Em7</sub>\n";
+    echo "I can show you everything, yeah\n";
+    echo "          <sub>Fmaj7</sub>\n";
+    echo "And we're on our way to glory\n";
+    echo "                          <sub>F#m7b5</sub>\n";
+    echo "Where the show won't ever end\n";
+    echo "        <sub>G7</sub>\n";
+    echo "And the encore lasts forever\n";
+    echo "         <sub>G7sus4            G7</sub>\n";
+    echo "And it's time we're due to spend\n";
+    echo " \n";
+    echo "<sub>Fmaj9    Fmmaj9</sub>\n";
+    echo "\n";
+    echo "\n";
+    echo "<sub>C</sub>\n";
+    echo "Spending the years together\n";
+    echo "                    <sub>Em7</sub>\n";
+    echo "Growing older every day (Every day)\n";
+    echo "          <sub>Fmaj7</sub>\n";
+    echo "I feel at home when I'm around you\n";
+    echo "                     <sub>F#m7b5</sub>\n";
+    echo "And I'll gladly say again\n";
+    echo "           <sub>G7</sub>\n";
+    echo "I hope the encore lasts forever\n";
+    echo "            <sub>G7sus4         G7       Fmaj9</sub>\n";
+    echo "Now there's time for us to spend\n";
+    echo "                              <sub>Fmmaj9</sub>\n";
+    echo "And it's sublime with you, my friend\n";
+    echo "<sub>C</sub>\n";
+    echo "This right here still feels like a honeymoon\n";
+    echo "                <sub>Em7</sub>\n";
+    echo "When you say my name, nothing's changed\n";
+    echo "                          <sub>Fmaj7</sub>\n";
+    echo "I'm still a boy inside my thoughts\n";
+    echo "              <sub>F#m7b5        G7</sub>\n";
+    echo "Am I meant to understand my faults?\n";
+    echo "\n";
+    echo "\n";
+    echo "                <sub>C</sub>\n";
+    echo "I don't think so\n";
+    echo "                                       <sub>Em7</sub>\n";
+    echo "I don't think I'm meant to understand myself\n";
+    echo "<sub>E7sus2    Fmaj7</sub>\n";
+    echo "Maybe you do\n";
+    echo "\n";
+    echo "And that's good for you\n";
+    echo "<sub>F#m7b5   G7</sub>\n";
+    echo "Maybe in time\n";
+    echo "          <sub>G7sus4</sub>\n";
+    echo "Maybe one day\n";
+    echo "<sub>G7           C</sub>\n";
+    echo "I'll do the same\n";
+    echo "        <sub>Em7</sub>\n";
+    echo "ooo OOO ooo\n";
+    echo " \n";
+    echo "(play verse chords for Ooos)...\n";
+    echo " \n";
+    echo "             <sub>G7</sub>\n";
+    echo "(I'll do the same\n";
+    echo "            <sub>G7sus4</sub>\n";
+    echo "I'll do the same\n";
+    echo "<sub>G7</sub>\n";
+    echo "I'll do the\n";
+    echo "<sub>Fmaj7   Em7   Dm7   D7</sub>\n";
+    echo "same...\n";
+    echo "<sub>Gm7   F#maj7   Fmaj7</sub>\n";
+    echo "\n";
+    echo "\n";
+    echo "            <sub>Fmaj7</sub>\n";
+    echo "I'll do the same as you\n";
+    echo "             <sub>Em7</sub>\n";
+    echo "I'll try and hold it up\n";
+    echo "       <sub>Dm7</sub>\n";
+    echo "Soon I hope\n";
+    echo "<sub>              D7             Gm7   Gbmaj7   Fmaj7</sub>\n";
+    echo "Or as soon as I'm old enough\n";
+    echo " \n";
+    echo "<sub>Bb   D7/A   Gm   G</sub>\n";
+    echo " \n";
+    echo "<sub>Cm             B      Bb</sub>\n";
+    echo "(Old enough to understand)\n";
+    echo " \n";
+    echo "<sub>Bb   D7/A   Gm   G</sub>\n";
+    echo " \n";
+    echo "<sub>Cm   B</sub>\n";
+    echo " \n";
+    echo "<sub>Bb                 Bb   D7/A   Gm   G</sub>\n";
+    echo "Old enough to  understand)\n";
+    echo " \n";
+    echo "<sub>Cm   B   Bb</sub>\n";
+    echo " \n";
+    echo "<sub>Bb   D7/A   Gm   G\n";
+    echo "Cm   B    Bb\n";
+    echo "\n";
+    echo "Cm   B    Bb\n";
+    echo "Cm   B</sub>\n";
+    echo "\n";
+    echo "\n";
+    echo "<sub>Bb                                  Dm7  Dm7b5</sub>\n";
+    echo "Stay forever, you know more than anyone (Yeah, whoa)\n";
+    echo "         <sub>Cm7</sub>\n";
+    echo "And it's you that knows my darkness\n";
+    echo "\n";
+    echo "And you know my bedroom needs\n";
+    echo "          <sub>Bbmaj7</sub>\n";
+    echo "You could blast me and my secrets\n";
+    echo "                                 <sub>Bb7</sub>\n";
+    echo "Because there's probably just no need\n";
+    echo "<pre>";
+    ?>
+</body>
+</html>
