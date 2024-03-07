c458ecdcfd45fa80fbc237ff6945d71b849dd2d8
CarlMadrigal <carlmadrigal65@gmail.com>
2024-03-07 20:43:29 +0800

    Song5.php Added

---
+++ song5.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sugar</title>
+    <style>
+        body {
+            font-family: Arial;
+            background-color: #f0f0f0;
+        }
+        h1, h5 {
+            margin-top: 20px;
+        }
+        hr {
+            border: 3px solid #ccc;
+        }
+    </style>
+</head>
+<body>
+    <h1>Sugar</h1>
+    <h5>By: Brockhampton</h5>
+    <hr>
+    <?php
+    echo "<pre>";
+    echo "<sub>Am                        G</sub>\n";
+    echo "Spendin' all my nights alone, waiting for you to call me\n";
+    echo "<sub>                C                     Bm</sub>\n";
+    echo "You're the only one I want by my side when I fall asleep\n";
+    echo "<sub>                 G                             D</sub>\n";
+    echo "Tell me what I'm waiting for, tell me what I'm waiting for\n";
+    echo "<sub>            C</sub>\n";
+    echo "I know it's hard but we need each other\n";
+    echo "<sub>          Bm</sub>\n";
+    echo "Know it's hard but we need each other\n";
+    echo "\n";
+    echo "\n";
+    echo "<sub>Em                                 G</sub>\n";
+    echo "I move mountains on my own, don't need nobody help\n";
+    echo "<sub>            C                                          Bm</sub>\n";
+    echo "Change your mind when I change my life, better start believing in myself\n";
+    echo "<sub>               Em                                       G</sub>\n";
+    echo "And we all out lookin' for, lookin' for God so we never see it in ourself\n";
+    echo "<sub>                  C</sub>\n";
+    echo "Should divine intervention move itself?\n";
+    echo "<sub>             Bm</sub>\n";
+    echo "It's hard to tell what the prayer compel\n";
+    echo "<sub>                Em                     G</sub>\n";
+    echo "You can find me dancing in between the raindrops\n";
+    echo "\n";
+    echo "Tryna find a way to make the pain stop\n";
+    echo "<sub>C</sub>\n";
+    echo "Overtime, on the graveyard\n";
+    echo "<sub>Bm</sub>\n";
+    echo "Got a nigga feeling brainwashed\n";
+    echo "<sub>   Em</sub>\n";
+    echo "My instability's trademark\n";
+    echo "<sub>G</sub>\n";
+    echo "Copy-written in all my decisions\n";
+    echo "<sub>        C</sub>\n";
+    echo "This is not supposed to be a way of living\n";
+    echo "<sub>        Bm</sub>\n";
+    echo "Tear my temple down into a prison\n";
+    echo "\n";
+    echo "\n";
+    echo "<sub>                Am                        G</sub>\n";
+    echo "Spendin' all my nights alone, waiting for you to call me\n";
+    echo "<sub>                C                     Bm</sub>\n";
+    echo "You're the only one I want by my side when I fall asleep\n";
+    echo "<sub>                 G                             D</sub>\n";
+    echo "Tell me what I'm waiting for, tell me what I'm waiting for\n";
+    echo "<sub>            C</sub>\n";
+    echo "I know it's hard but we need each other\n";
+    echo "<sub>          Bm</sub>\n";
+    echo "Know it's hard but we need each other\n";
+    echo "\n";
+    echo "\n";
+    echo "<sub>      Em</sub>\n";
+    echo "Yeah, back on Vincent with the braces on\n";
+    echo "<sub>        G</sub>\n";
+    echo "Used to slide out the back without the neighbors knowin'\n";
+    echo "<sub>C</sub>\n";
+    echo "Pose for the picture with the pearly whites\n";
+    echo "<sub>     Bm</sub>\n";
+    echo "Dead lens zoomin' in, catching all my stripes\n";
+    echo "<sub>Em</sub>\n";
+    echo "Used to trade a joint for some molly\n";
+    echo "                      <sub>G</sub>\n";
+    echo "And she gave me all I need for the night, 40 suffice\n";
+    echo "<sub>C</sub>\n";
+    echo "Morally alright but I need some advice\n";
+    echo "      <sub>B</sub>\n";
+    echo "And I know that I'm acting foolish\n";
+    echo "<sub>Em</sub>\n";
+    echo "Could you pick me up around noon-ish?\n";
+    echo "<sub>G</sub>\n";
+    echo "Half a blunt, yeah, we coolin'\n";
+    echo "<sub>C</sub>\n";
+    echo "Twist it up, putting on OutKast\n";
+    echo "           <sub>Bm</sub>\n";
+    echo "I'm the taxi, and yeah, we cruisin'\n";
+    echo "\n";
+    echo "\n";
+    echo "           <sub>Em                G</sub>\n";
+    echo "But when I love you right, I love you right\n";
+    echo "<sub>C                 D</sub>\n";
+    echo "I'm by your side\n";
+    echo "                         <sub>Em                   G</sub>\n";
+    echo "But I'll make it right, baby, I want you to know\n";
+    echo "     <sub>C                      Bm</sub>\n";
+    echo "I'ma be there for you, I'ma make you see that\n";
+    echo "  <sub>Em    D G</sub>\n";
+    echo "I want you,\n";
+    echo "<sub>C     Bm Am</sub>\n";
+    echo "I want you\n";
+    echo "\n";
+    echo "\n";
+    echo "                <sub>Am                        G</sub>\n";
+    echo "Spendin' all my nights alone, waiting for you to call me\n";
+    echo "<sub>                C                     Bm</sub>\n";
+    echo "You're the only one I want by my side when I fall asleep\n";
+    echo "<sub>                 G                             D</sub>\n";
+    echo "Tell me what I'm waiting for, tell me what I'm waiting for\n";
+    echo "<sub>            C</sub>\n";
+    echo "I know it's hard but we need each other\n";
+    echo "<sub>          Bm</sub>\n";
+    echo "Know it's hard but we need each other\n";
+    echo "\n";
+    echo "\n";
+    echo "<sub>C</sub>\n";
+    echo "Back and forth\n";
+    echo "<sub>Am                               Em</sub>\n";
+    echo "I'll take that if that's all you asking for\n";
+    echo "<sub>G                      C</sub>\n";
+    echo "With my legs up on the dashboard\n";
+    echo "<sub>Am                            Em          G</sub>\n";
+    echo "Only thing in my pocket is my passport, pa-passport\n";
+    echo "<sub>C</sub>\n";
+    echo "Back and forth\n";
+    echo "<sub>Am                                Em</sub>\n";
+    echo "I'll take that if that's all you asking for\n";
+    echo "<sub>G                      C</sub>\n";
+    echo "With my legs up on the dashboard\n";
+    echo "<sub>Am                            Em          G</sub>\n";
+    echo "Only thing in my pocket is my passport, pa-passport\n";
+    echo "<sub>C</sub>\n";
+    echo "Back and forth\n";
+    echo "<sub>Am                               Em</sub>\n";
+    echo "I'll take that if that's all you asking for\n";
+    echo "<sub>G                      C</sub>\n";
+    echo "With my legs up on the dashboard\n";
+    echo "<sub>Am                            Em          G</sub>\n";
+    echo "Only thing in my pocket is my passport, pa-passport\n";
+    echo "\n";
+    echo "\n";
+    echo "           <sub>C                 Am</sub>\n";
+    echo "So, do you love me, love me, love me?\n";
+    echo "       <sub>Em                G</sub>\n";
+    echo "Do you love me, love me, love me?\n";
+    echo "       <sub>C                 Am</sub>\n";
+    echo "Do you love me, love me, love me?\n";
+    echo "       <sub>Em                G</sub>\n";
+    echo "Do you love me, love me? Oh\n";
+    echo "       <sub>C                 Am</sub>\n";
+    echo "Do you love me, love me, love me?\n";
+    echo "       <sub>Em                G</sub>\n";
+    echo "Do you love me, love me, love me?\n";
+    echo "       <sub>C                 Am</sub>\n";
+    echo "Do you love me, love me, love me?\n";
+    echo "       <sub>C                 G</sub>\n";
+    echo "Do you love me, love me? Oh\n";
+    echo "</pre>";
+    ?>
+</body>
+</html>
