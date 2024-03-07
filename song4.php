1020f599ed6699e5afa74b3d3d7aea895b20c38e
CarlMadrigal <carlmadrigal65@gmail.com>
2024-03-07 20:43:18 +0800

    Song4.php Added

---
+++ song4.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Starboy</title>
+    <style>
+        body {
+            font-family: Arial;
+            background-color: #f0f0f0;
+        }
+        h1, h5 {
+            margin-top: 20px;
+        }
+        hr {
+            border: 3px solid #ccc;
+        }
+    </style>
+</head>
+<body>
+    <h1>Starboy</h1>
+    <h5>By: The Weeknd</h5>
+    <hr>
+    <?php
+    echo "<pre>";
+    echo "<sub>Intro</sub>\n";
+    echo "<sub>Em   D   C   D</sub>\n";
+    echo "\n";
+    echo "\n";
+    echo "<sub>Em                                   D</sub>\n";
+    echo "  I'm tryna put you in the worst mood, ah\n";
+    echo "<sub>                       D            C</sub>\n";
+    echo "  P1 cleaner than your church shoes, ah\n";
+    echo "<sub>                                      D</sub>\n";
+    echo "  Milli point 2 just to hurt you, ah\n";
+    echo "<sub>                Bm7               Em</sub>\n";
+    echo "  All red lamb just to tease you, ah\n";
+    echo "\n";
+    echo "\n";
+    echo "<sub>                                        D</sub>\n";
+    echo "  None of these toys on the lease too, ah\n";
+    echo "<sub>                        D               C</sub>\n";
+    echo "  Made your whole year in a week too, yah\n";
+    echo "<sub>                                      D</sub>\n";
+    echo "  Main chick out of your league too, ah\n";
+    echo "<sub>                           Bm7        Em</sub>\n";
+    echo "  Side chick out of your league too, ah\n";
+    echo "\n";
+    echo "\n";
+    echo "<sub>                                D</sub>\n";
+    echo "  House so empty need a centerpiece\n";
+    echo "<sub>                   D                  C</sub>\n";
+    echo "  Twenty racks a table cut from ebony\n";
+    echo "<sub>                                    D</sub>\n";
+    echo "  Cut that ivory into skinny pieces\n";
+    echo "<sub>                               Bm7</sub>\n";
+    echo "  Then she clean it with her face but\n";
+    echo "<sub>                 Em</sub>\n";
+    echo "  I love my baby...\n";
+    echo "                                    <sub>D</sub>\n";
+    echo "  You talking money need a hearing aid\n";
+    echo "<sub>                       D                 C</sub>\n";
+    echo "  You talking bout me I don't see the shade\n";
+    echo "<sub>                                   Bm</sub>\n";
+    echo "  Switch up my style I take any lane\n";
+    echo "<sub>                                  Em</sub>\n";
+    echo "  I switch up my cup I kill any pain\n";
+    echo "\n";
+    echo "\n";
+    echo "   <sub>Em              D</sub>\n";
+    echo "  (Ha-ha-ha-ha-ha-ha-ha-ha-ha-ha)\n";
+    echo "                     <sub>C</sub>\n";
+    echo "  Look what you've done!\n";
+    echo "               <sub>D</sub>\n";
+    echo "  (Ha-ha-ha-ha-ha-ha-ha-ha-ha-ha)\n";
+    echo "<sub>Bm7                           Em</sub>\n";
+    echo "  Don't you know that I'm a Starboy\n";
+    echo "                <sub>D</sub>\n";
+    echo "  (Ha-ha-ha-ha-ha-ha)\n";
+    echo "                     <sub>C</sub>\n";
+    echo "  Look what you've done!\n";
+    echo "               <sub>D</sub>\n";
+    echo "  (Ha-ha-ha-ha-ha-ha-ha-ha-ha-ha)\n";
+    echo "<sub>Bm7                           Em</sub>\n";
+    echo "  Don't you know that I'm a Starboy\n";
+    echo "\n";
+    echo "\n";
+    echo "<sub>Em                                      D</sub>\n";
+    echo "  Everyday your brother try to test me, ah\n";
+    echo "<sub>                                       C</sub>\n";
+    echo "  Everyday your brother try to end me, ah\n";
+    echo "<sub>                                D</sub>\n";
+    echo "  Pull off in that roadster sv, ah\n";
+    echo "<sub>                          Bm7        Em</sub>\n";
+    echo "  Pockets over weight getting hefty, ah\n";
+    echo "\n";
+    echo "\n";
+    echo "<sub>Em                                     D</sub>\n";
+    echo "  Coming for the king that's a far cry I\n";
+    echo "<sub>                                C</sub>\n";
+    echo "  I come alive in the fall time I\n";
+    echo "<sub>                                      D</sub>\n";
+    echo "  No competition I don't really listen\n";
+    echo "<sub>                      Bm7</sub>\n";
+    echo "  I'm in the blue mulsanne bumping\n";
+    echo "<sub>             Em</sub>\n";
+    echo "  new edition\n";
+    echo "\n";
+    echo "\n";
+    echo "                               <sub>D</sub>\n";
+    echo "  House so empty need a centerpiece\n";
+    echo "<sub>                  D                  C</sub>\n";
+    echo "  Twenty racks a table cut from ebony\n";
+    echo "<sub>                                   D</sub>\n";
+    echo "  Cut that ivory into skinny pieces\n";
+    echo "<sub>                              Bm7</sub>\n";
+    echo "  Then she clean it with her face but\n";
+    echo "<sub>                 Em</sub>\n";
+    echo "  I love my baby...\n";
+    echo "                                      <sub>D</sub>\n";
+    echo "  You talking money need a hearing aid\n";
+    echo "<sub>                      D                 C</sub>\n";
+    echo "  You talking bout me I don't see the shade\n";
+    echo "<sub>                                  Bm</sub>\n";
+    echo "  Switch up my style I take any lane\n";
+    echo "<sub>                                 Em</sub>\n";
+    echo "  I switch up my cup I kill any pain\n";
+    echo "\n";
+    echo "\n";
+    echo "   <sub>Em              D</sub>\n";
+    echo "  (Ha-ha-ha-ha-ha-ha-ha-ha-ha-ha)\n";
+    echo "                     <sub>C</sub>\n";
+    echo "  Look what you've done!\n";
+    echo "               <sub>D</sub>\n";
+    echo "  (Ha-ha-ha-ha-ha-ha-ha-ha-ha-ha)\n";
+    echo "<sub>Bm7                           Em</sub>\n";
+    echo "  Don't you know that I'm a Starboy\n";
+    echo "                <sub>D</sub>\n";
+    echo "  (Ha-ha-ha-ha-ha-ha)\n";
+    echo "                     <sub>C</sub>\n";
+    echo "  Look what you've done!\n";
+    echo "               <sub>D</sub>\n";
+    echo "  (Ha-ha-ha-ha-ha-ha-ha-ha-ha-ha)\n";
+    echo "<sub>Bm7                           Em</sub>\n";
+    echo "  Don't you know that I'm a Starboy\n";
+    echo "\n";
+    echo "\n";
+    echo "                <sub>D</sub>\n";
+    echo "  (Ha-ha-ha-ha-ha-ha-ha-ha-ha-ha)\n";
+    echo "                              <sub>C</sub>\n";
+    echo "  Don't you know that I'm a Starboy\n";
+    echo "         <sub>D                     Bm7</sub>\n";
+    echo "  (Ha-ha-ha-ha-ha-ha-ha-ha-ha-ha)\n";
+    ?>
+</body>
+</html>
