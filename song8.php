34109aed1835b5c81f8f4b027514358a302178c3
CarlMadrigal <carlmadrigal65@gmail.com>
2024-03-07 20:44:21 +0800

    Song8.php Added

---
+++ song8.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Leonora</title>
+    <style>
+        body {
+            font-family: Arial;
+            background-color: #f0f0f0;
+        }
+        h1, h5 {
+            margin-top: 20px;
+        }
+        hr {
+            border: 3px solid #ccc;
+        }
+    </style>
+</head>
+<body>
+    <h1>Leonora</h1>
+    <h5>By: Sugarcane</h5>
+    <hr>
+    <?php
+    echo "<pre>";
+    echo "FM7 Em D Gsus4 G\n";
+    echo "\n";
+    echo "\n";
+    echo "      C           Am   G\n";
+    echo "‘Tong alay kong hara - na\n";
+    echo "C         Am   G\n";
+    echo "Para sa dala - gang\n";
+    echo "  C              Em\n";
+    echo "Walang kasing ganda\n";
+    echo "     FM7           Gsus4 G\n";
+    echo "Amoy rosas ang halimu  - yak\n";
+    echo "     FM7      Em7\n";
+    echo "Kung nanaisin ng\n";
+    echo "   FM7            Em7\n";
+    echo "Tadhanang mapanlinlang\n";
+    echo "        D          Gsus4 G\n";
+    echo "‘Di hahayaang, mawala    pa\n";
+    echo "\n";
+    echo "\n";
+    echo "      C           Am  G\n";
+    echo "‘Tong liham na umaa - sang\n";
+    echo "  C             Am   G\n";
+    echo "Mata mo ang makaba - sa\n";
+    echo "   C            Em\n";
+    echo "Handang gawin lahat\n";
+    echo "       FM7           Gsus4 G\n";
+    echo "Maging pamilya’y liliga  - wan\n";
+    echo "   FM7            Em7\n";
+    echo "Ngayon lang nakadama\n";
+    echo "     FM7             Em7\n";
+    echo "Ng wagas na pagkamangha\n";
+    echo "          D           Gsus4 G\n";
+    echo "Hiling ko lang naman na\n";
+    echo "\n";
+    echo "\n";
+    echo "        FM7        C\n";
+    echo "Tayo na sanang dalawa\n";
+    echo "            FM7           C\n";
+    echo "Ang syang huli at ang umpisa\n";
+    echo "      FM7         C\n";
+    echo "Papatunayang ang unang\n";
+    echo "            FM7       C\n";
+    echo "Pag-ibig ay 'di mawawala\n";
+    echo "\n";
+    echo "\n";
+    echo "  C         Am G\n";
+    echo "Nakailang tula na,\n";
+    echo "    C          Am  G\n";
+    echo "Bat tila 'di napupuna?\n";
+    echo "    C            Em\n";
+    echo "Ang tangi kong hiling\n";
+    echo "         F                G\n";
+    echo "Hanggang dulo, ikaw ang kapiling\n";
+    echo "     FM7             C\n";
+    echo "Kung puwede lang hanggang\n";
+    echo "     FM7        C\n";
+    echo "Pang magpakailanman\n";
+    echo "           D                        Gsus4 G\n";
+    echo "Hinding hindi na papakawalan, kailanman\n";
+    echo "\n";
+    echo "\n";
+    echo "             FM7                      C\n";
+    echo "Ang dating tamis ng pagsasama, nasa'n na? (Hinahanap-hanap ka)\n";
+    echo "                 FM7                     C\n";
+    echo "Ba't sa'ting dal’wa, ako na lang ang natira? (Sana’y magkita pa)\n";
+    echo "           FM7                     C\n";
+    echo "Tinig mong kay ganda, maririnig pa ba?\n";
+    echo "                     D            Gsus4   G\n";
+    echo "Handang tahaking mag-isa, kahit wala   ka na\n";
+    echo "\n";
+    echo "\n";
+    echo "F#M7 C# x3\n";
+    echo "D# G#sus4 G#\n";
+    echo "\n";
+    echo "\n";
+    echo "               F#M7                   C#\n";
+    echo "Kung nasa'n ka man, nawa ay masaya ka na\n";
+    echo "         F#M7                  C#\n";
+    echo "Kahit na 'di na tayo magsasama pa\n";
+    echo "                F#M7                     C#\n";
+    echo "Dinggin mo lang ang hiling na mag-iingat ka\n";
+    echo "                  D#\n";
+    echo "Oh, Leonora kong sinta\n";
+    echo "G#sus4 G#\n";
+    echo "Ahh\n";
+    echo "\n";
+    echo "\n";
+    echo "F#M7 Fm7 A#7\n";
+    echo "D# G#sus4 G# F#\n";
+    echo "</pre>";
+    ?>
+</body>
+</html>
